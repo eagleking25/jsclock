67c2b3d153e09d1639914f791172a7246e67fc43
Erubure Ejiro david <davidejiro49a@gmail.com>
2020-07-06 03:22:17 +0100

    Last commit

---
+++ timer.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    let sec = document.querySelector('.sec-hand');
+    let min = document.querySelector('.min-hand');
+    let hrs = document.querySelector('.hrs-hand');
+    setInterval(() => {
+        const date = new Date();
+        sec.style.transform = `rotate(${date.getSeconds()*6+90}deg)`;
+        min.style.transform = `rotate(${date.getMinutes()*6+90}deg)`;
+        hrs.style.transform = `rotate(${date.getHours()*360/12+90}deg)`;
+        console.log(date.getHours());
+    }, 1000);
+})
